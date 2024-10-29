565633aa415f0a5076ea58fde0c0456e17864c7f
薑黃 <jhuanglululu@iPad-Air-5.local>
2024-10-29 12:22:20 -0400

    Initial commit

---
+++ src/main/java/net/jhuanglululu/jhuanglululufactory/JhuanglululuFactoryClient.java
@@ -0,0 +1,10 @@
+package net.jhuanglululu.jhuanglululufactory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JhuanglululuFactoryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/jhuanglululu/jhuanglululufactory/JhuanglululuFactoryDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jhuanglululu.jhuanglululufactory;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JhuanglululuFactoryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
